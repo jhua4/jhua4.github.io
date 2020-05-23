79417cc49db6573d9d003b0af1869029fd793f40
jhua4 <jhua82196@gmail.com>
2020-05-23 09:50:09 -0700

    center text, reverse order of experience

--- scripts.js
+++ scripts.js
@@ -16,9 +16,8 @@ function navHome() {
   document.getElementById('main-content').innerHTML = `
     <div class="banner">
       <h2>Full Stack Web Developer / Generalist</h2>
-      <p>Hi, I'm Joshua Hua. If you're looking for examples of my work, you can check out my latest project at <a href="https://github.com/jhua4/needyd" target="_blank">https://github.com/jhua4/needyd</a></p>
+      <p>Hi, I'm Joshua Hua. I'm a software engineer with experience in full stack, but I like to experiment and work on new things! Check out my latest project at <a href="https://github.com/jhua4/needyd" target="_blank">https://github.com/jhua4/needyd</a></p>
     </div>
-    <img src="server.png" alt="Code" class="image-center">
   `;
 }
 
@@ -29,23 +28,19 @@ function navExperience() {
       <div class="timeline color1">
         <div class="item left">
           <div class="content">
-            <b class="color1">September 2016 - May 2018</b><br>
-            Software Engineer, Alpha Connect<br><br>
-            Added new features to .NET desktop application, and built new applications.<br>
-            Installed and integrated SQL server for storage.<br>
-            Mentored a junior developer.
+            <b>October 2018 - Present</b><br>
+            Senior Software Developer, American Airlines<br><br>
+            Built two full stack applications used by frontline workers to order parts for aircraft maintenance.<br>
+	          Developed .NET Core WebAPI and Node.js backends, and Angular frontends.<br>
+	          Integrated with other backend services and SQL/Oracle databases.<br>
+	          Setup CI/CD pipeline, monitor application health using Dynatrace.<br>
+	          Work with other teams to facilitate development/testing and debug issues with data and infrastructure.
+
             <ul>
-              <li>Languages: C#, VB.NET</li>
-              <li>Frameworks: .NET Framework, WinForms</li>
+              <li>Languages: C#, TypeScript, JavaScript, HTML, Less, GraphQL</li>
+              <li>Libraries: Kendo UI for Angular, nrwl/nx</li>
+              <li>Frameworks: .NET Core, Angular 7, EF Core, Node.js, xUnit, Jasmine, Protractor</li>
             </ul>
-          </div>
-        </div>
-      </div>
-      <div class="timeline color1">
-        <div class="item right">
-          <div class="content">
-            <b>May 2018</b><br>
-            Graduated ASU, B.S. Computer Science
           </div>
         </div>
       </div>
@@ -64,20 +59,24 @@ function navExperience() {
         </div>
       </div>
       <div class="timeline color1">
+        <div class="item right">
+          <div class="content">
+            <b>May 2018</b><br>
+            Graduated ASU, B.S. Computer Science
+          </div>
+        </div>
+      </div>
+      <div class="timeline color1">
         <div class="item left">
           <div class="content">
-            <b>October 2018 - Present</b><br>
-            Senior Software Developer, American Airlines<br><br>
-            Built two full stack applications used by frontline workers to order parts for aircraft maintenance.<br>
-	          Developed .NET Core WebAPI and Node.js backends, and Angular frontends.<br>
-	          Integrated with other backend services and SQL/Oracle databases.<br>
-	          Setup CI/CD pipeline, monitor application health using Dynatrace.<br>
-	          Work with other teams to facilitate development/testing and debug issues with data and infrastructure.
-
+            <b class="color1">September 2016 - May 2018</b><br>
+            Software Engineer, Alpha Connect<br><br>
+            Added new features to .NET desktop application, and built new applications.<br>
+            Installed and integrated SQL server for storage.<br>
+            Mentored a junior developer.
             <ul>
-              <li>Languages: C#, TypeScript, JavaScript, HTML, Less, GraphQL</li>
-              <li>Libraries: Kendo UI for Angular, nrwl/nx</li>
-              <li>Frameworks: .NET Core, Angular 7, EF Core, Node.js, xUnit, Jasmine, Protractor</li>
+              <li>Languages: C#, VB.NET</li>
+              <li>Frameworks: .NET Framework, WinForms</li>
             </ul>
           </div>
         </div>
